87aa0937ae096211f590663d668c85aa9c55e23f
wangziwen <451413419@qq.com>
2018-10-08 21:09:56 +0800

    1.增加udp_server_proto工程； 2.增加udp_client_proto工程；

---
+++ examples/udp_client_proto.cpp
@@ -0,0 +1,43 @@
+#include <ip/client/udp_client.h>
+#include "proto_client_message_factory.h"
+#include <iostream>
+#include <utils.h>
+#include <thread>
+#include <chrono>
+#include "dmo.pb.h"
+#include <network/proto_network_convert.h>
+#ifdef GLOG_OUTPUT
+#include <config_glog.h>
+#endif
+using namespace happy::utils::network;
+using namespace std;
+using namespace happy::asio::ip;
+
+int main(int argc, char* argv[])
+{
+	// 默认创建线程池的大小为4。
+	//IoServicePool::singleton::Create(4); 
+#ifdef GLOG_OUTPUT
+	happy::utils::ConfigGlog(argv[0]);
+#endif
+	
+	auto const ip = "127.0.0.1";
+	auto const port = 1235;
+	auto udp_client = make_shared <UdpClient>(ip, port);
+	udp_client->SetMessageFactory(dynamic_pointer_cast <happy::asio::ip::MessageFactory>(make_shared <ProtoClientMessageFactory>("", true)));
+	thread t([]() { IoServicePool::singleton::GetInstance()->Run(); });
+	t.detach();
+	while (true)
+	{
+		cout << "please input send server string(enter to end): ";
+		char buffer[1024];
+		cin >> buffer;
+		auto request_info = make_shared <RequestInfo>();
+		request_info->set_info(buffer);
+		string out_buffer;
+		ProtoNetworkConvert::singleton::GetInstance()->ToNetwork(request_info, out_buffer);
+		udp_client->AsyncWrite(out_buffer);
+		this_thread::sleep_for(chrono::seconds(1));
+	}
+	return EXIT_SUCCESS;
+}
